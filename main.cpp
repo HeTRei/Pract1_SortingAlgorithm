1cb94f75ac71b73737ce42707480199a0f5ea8e8
HeTRei <mememanishereboiz@gmail.com>
2023-06-01 01:34:43 +0200

    Linear search: initial implementation. Binary search: initial implementation.

--- main.cpp
+++ main.cpp
@@ -57,18 +57,44 @@ void CombDescending(int *list, int NumberOfElements)
             }
     }
 }
+void LinearSearch(const long long *list, const long long NumberOfElements, long long ValueToFind)
+{
+    for (long long i = 0; i < NumberOfElements; i++)
+        if (list[i] == ValueToFind)
+            return;
+}
+void BinarySearch(const long long *list, const long long NumberOfElements, long long ValueToFind)
+{
+    long long MinIndex = 0;
+    long long MaxIndex = (NumberOfElements) - 1;
+    while (MinIndex < MaxIndex)
+    {
+        long long MidIndex = MinIndex + (MaxIndex - MinIndex)/2;
+        if (list[MidIndex] == ValueToFind)
+            return;
+        else if (list[MidIndex] < ValueToFind)
+            MinIndex = MidIndex + 1;
+        else
+            MaxIndex = MidIndex - 1;
+    }
+
+
+}
+
 int main()
 {
     //Setup
     srand(time(nullptr));
-    double time = 0;
+    double time;
     std::chrono::duration<double> time1;
+
+    //Sorting
     int n = 1000;
     int *MatrixA = new int [n];
     for (int i = 0; i < n; i++)
         MatrixA[i] = rand();
 
-    //Bubble Sorting
+    //Bubble Sort
     for (int i = 0; i < 10; i++)
     {
         if (i%2==0)
@@ -89,12 +115,10 @@ int main()
         }
 
     }
-    std::cout << "Worst average time for Bubble Sort = " << (time/10) << std::endl;
-
-    //Cleanup
+    std::cout << "Worst average time for Bubble sort = " << (time/10) << std::endl;
     time = 0;
 
-    //Comb sorting
+    //Comb sort
     for (int i = 0; i < 10; i++)
     {
         if (i%2==0)
@@ -115,8 +139,39 @@ int main()
         }
 
     }
-    std::cout << "Worst average time for Comb Sort = " << (time/10);
+    std::cout << "Worst average time for Comb sort = " << (time/10) << std::endl;
+    time = 0;
+
+    //Search
+    auto* D = new long long[1000000];;
+    for (long long i = 0; i < 1000000; i++)
+        D[i] = i+1;
+    long long ValueToFind = 387235;
+
+    //Linear search
+    for (int i = 0; i < 10; i++)
+    {
+        auto start1 = std::chrono::steady_clock::now();
+        LinearSearch(D, 1000000, ValueToFind);
+        auto end1 = std::chrono::steady_clock::now();
+        time1 = end1 - start1;
+        time += time1.count();
+    }
+    std::cout << "Worst average time for Linear search = " << (time/10) << std::endl;
+    time = 0;
+
+    //Binary search
+    for (int i = 0; i < 10; i++)
+    {
+        auto start1 = std::chrono::steady_clock::now();
+        BinarySearch(D, 1000000, ValueToFind);
+        auto end1 = std::chrono::steady_clock::now();
+        time1 = end1 - start1;
+        time += time1.count();
+    }
+    std::cout << "Worst average time for Binary search = " << (time/10);
 
     delete[] MatrixA;
+    delete[] D;
     return 0;
 }
